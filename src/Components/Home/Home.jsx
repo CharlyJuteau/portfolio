6e4f2e9c00fbd04acab03dc77e9af538a2f24502
CharlyJuteau <Charly.juteau4@gmail.com>
2022-12-24 16:35:56 +0100

    Created burger menu

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './home.css'
+
+export default function Home() {
+    return (
+        <div className="homePage">
+            <p className="textIntro">
+                Hey! I'm Charly and Welcome to my little website!
+            </p>
+        </div>
+    )
+}
---
+++ src/Components/Navbar/Rightnav/Rightnav.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+
+  li {
+    padding: 18px 10px;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0d2538;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const Rightnav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li>Home</li>
+      <li>Work</li>
+      <li>Projects</li>
+      <li>Contact Me</li>
+    </Ul>
+  );
+};
+
+export default Rightnav;
